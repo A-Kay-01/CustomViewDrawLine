02e7e8e5227eda5e1ca428beb48ef61a7a5db3eb
User <letstalk.akay@gmail.com>
2021-01-18 15:36:10 +0530

    second Commit

---
+++ app/src/main/java/com/example/customviewdrawline/DrawPath.kt
@@ -0,0 +1,10 @@
+package com.example.customviewdrawline
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DrawPath @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+}
